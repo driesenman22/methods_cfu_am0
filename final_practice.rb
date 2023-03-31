7b7252e118667afdb8e05e4fd2829c55468efdc8
Dylan Riesenman <djriesenman@gmail.com>
2023-03-31 13:47:44 -0600

    Add partial final practice CFU

--- final_practice.rb
+++ final_practice.rb
@@ -6,22 +6,48 @@
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+# def greet_your_pal
+#     return "Well hey there, pal!"
+# end
+# puts greet_your_pal
 
+# What is the return value of your method?
+# The return value is 'Well hey there, pal!''
+# How many arguments did you pass your method?
+# 0
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
+# def custom_greeting(name)
+#     return "Well I'll be... if it isn't my good ol' buddy #{name}!"
+# end
+
+# puts custom_greeting("Jim Bob")
+
 # What is the return value of your method?
+# "Well I'll be... if it isn't my good ol' buddy Jim Bob!"
 # How many arguments did you pass your method?
+# 1
 # What data type was your argument(s)?
+# String
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
+def greet_person(first_name, middle_name, last_name)
+    full_name = "#{first_name} #{middle_name} #{last_name}"
+    return "Well I'll be... if it isn't my good ol' buddy who I refer to by his full name, #{full_name}!"
+end
+
+puts greet_person("Jim", "Bob", "Johnson")
+
+
 # What is the return value of your method?
+# Well I'll be... if it isn't my good ol' buddy who I refer to by his full name, Jim Bob Johnson!
 # How many arguments did you pass your method?
+# 3
 # What data type was your argument(s)?
+# String
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
@@ -35,14 +61,14 @@
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
-check_stock(4, "Coffee");
-# => "Coffee is stocked"
+# check_stock(4, "Coffee");
+# # => "Coffee is stocked"
 
-check_stock(3, "Tortillas");
-# => "Tortillas - running LOW"
+# check_stock(3, "Tortillas");
+# # => "Tortillas - running LOW"
 
-check_stock(0, "Cheese");
-# => "Cheese - OUT of stock!"
+# check_stock(0, "Cheese");
+# # => "Cheese - OUT of stock!"
 
-check_stock(1, "Salsa");
-# => "Salsa - running LOW"
+# check_stock(1, "Salsa");
+# # => "Salsa - running LOW"
